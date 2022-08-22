45127bb901d7b4037d90a74240aa218402593da0
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2022-08-22 18:53:20 -0500

    added a simple celsius to fahrenheit converter to get the repo started

---
+++ ConvertCelsius/ConvertCelsius.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ConvertCelsius {
+	public static void main(String[] args){
+		//create scanner for input
+		Scanner input = new Scanner(System.in);
+
+		//prompt user and recieve input
+		System.out.println("What is the temp in celsius?");
+		double temp = input.nextDouble();
+
+		//calculate and output the results
+		double fahrenheit = (9.0/5) * temp + 32;
+		System.out.println(temp + " degrees celsius is " + 
+			fahrenheit + " degrees fahrenheit");
+
+	}
+}
