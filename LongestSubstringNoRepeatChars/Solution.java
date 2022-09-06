86fe51be092e07672248efb5adcfba6491e86000
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2022-09-05 20:29:45 -0500

    fixed substring answer, sadly needs nested loop

--- LongestSubstringNoRepeatChars/Solution.java
+++ LongestSubstringNoRepeatChars/Solution.java
@@ -1,22 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        //check if input is valid
+        if(s == null || s.length() == 0) return 0;
+        if(s.length() == 1) return 1;
+
+        //variables to track our max substring, pointer, and substring
+        //the first pointer will be the loop variable
         int longestSubString = 0;
-        String subString = "";
-        String resultSubString = "";
+        int jPointer = 0;
+        Set<Character> subString = new HashSet<Character>();
+
         for(int i = 0; i < s.length(); i++){
-            if(i == 0){
-                subString += s.charAt(i);
-            }
-            else if(!subString.toLowerCase().contains("" + s.charAt(i))){
-                subString += s.charAt(i);
+            
+            //if the character is already in the set, remove the character 
+            //and advance second pointer
+            //otherwise add the character to the set
+            while(subString.contains(s.charAt(i))){
+                subString.remove(s.charAt(jPointer));
+                jPointer++;
             }
             
-            if(subString.length() > resultSubString.length()){
-                resultSubString = subString;
-                subString = "";
-                longestSubString = resultSubString.length();
-            }
+            subString.add(s.charAt(i));
+            
+
+            //update longestSubString if neccessary
+            longestSubString = Math.max(longestSubString, subString.size());
         }
+
         return longestSubString;
     }
 }
