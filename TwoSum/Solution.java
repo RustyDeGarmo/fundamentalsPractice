031e438d68da04fd1c7a7ca0460dfcf7bf702d6b
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2022-09-09 16:44:43 -0500

    updated two sum for hash map, trade memory for faster execution

--- TwoSum/Solution.java
+++ TwoSum/Solution.java
@@ -1,18 +1,20 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+                
         int[] result = new int[2];
+        HashMap<Integer, Integer> visitedNums = new HashMap<Integer, Integer>();
+        visitedNums.put(nums[0], 0);
         
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(j != i){
-                    if(nums[i] + nums[j] == target){
-                        result[0] = i;
-                        result[1] = j;
-                        
-                        
-                    }
-                }
+        for(int i = 1; i < nums.length; i++){
+            int differenceValue = target - nums[i];
+            
+            if(visitedNums.containsKey(differenceValue)){
+                        result[0] = visitedNums.get(differenceValue);
+                        result[1] = i; 
+            }else{
+                visitedNums.put(nums[i], i);
             }
         }
         return result;
