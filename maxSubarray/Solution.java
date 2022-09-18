c7d535c69f5e8ee7813e5766b92433929912eeea
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2022-09-17 21:39:38 -0500

    fixed to allow for negative value results if no positives exist in the array

--- maxSubarray/Solution.java
+++ maxSubarray/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxSubArray(int[] nums) {
         
         
-        int sum = 0;
+        int sum = Integer.MIN_VALUE;
         int tempSum = 0;
         
         for(int i = 0; i < nums.length; i++){
