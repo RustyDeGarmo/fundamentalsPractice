976ac8c07bb1f1875ee9145ec328cf83234b2f65
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2022-09-17 21:31:40 -0500

    added solution to the maximum subarray problem

---
+++ maxSubarray/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        
+        int sum = 0;
+        int tempSum = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            //sum the nums array
+            tempSum += nums[i];
+            
+            //if the current subarray sum is greater than our result sum, 
+            //update the result sum
+            if(tempSum > sum){
+                sum = tempSum;
+            }
+            
+            //if the sum reaches zero or negative we're starting a new
+            //subarray. As long as the value is positive it will add to
+            //the value of our subarray
+            if(tempSum < 0){
+                tempSum = 0;
+            }
+        }
+        
+        return sum;
+        
+        
+    }
+}
