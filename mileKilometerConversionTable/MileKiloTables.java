6939f496db6e9162f7636288064c744117eaab23
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2022-09-21 19:59:10 -0500

    finished milesToKiloConversionTables.

--- mileKilometerConversionTable/MileKiloTables.java
+++ mileKilometerConversionTable/MileKiloTables.java
@@ -10,13 +10,13 @@ public class MileKiloTables{
     public static void main(String[] args){
         
         //starting kilometers
-        int kilometers = 25;
+        int kilometers = 20;
 
         //We need to print the first line manually
         System.out.println("Miles\t\tKilometers\t|\tKilometers\tMiles");
 
         //we'll loop 10 times and use our loop variable for our miles
-        for(int i = 1; i < 10; i++){
+        for(int i = 1; i < 11; i++){
             System.out.println(
                 i + "\t\t" + mileToKilo(i) + "\t\t|\t" + kilometers + 
                 "\t\t" + kiloToMile(kilometers));
@@ -25,12 +25,14 @@ public class MileKiloTables{
     }
 
 
-    public double mileToKilo(double miles){
-        return (int)(miles * 1600) / 1000;
+    public static double mileToKilo(double miles){
+        //for whatever reason this isn't rounding as I would like no 
+        //matter what I try here and I don't want to use a formatted string
+        return (miles * 1600) / 1000.0;
     }
 
-    public double kiloToMile(double kilos){
-        return (int)(kilos / 1600) * 1000;
+    public static double kiloToMile(double kilos){
+        return (int)(kilos * 0621.37) / 1000.0;
     }
 }
 
