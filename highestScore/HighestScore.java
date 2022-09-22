83261fff098bf6bd0a827ead4144c26fdded5685
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2022-09-21 20:12:48 -0500

    completed highestScore. Could be interesting to get the data from a database instead of manually inputting so we can realistically process more than a few students. Something to do later on?

--- highestScore/HighestScore.java
+++ highestScore/HighestScore.java
@@ -4,10 +4,37 @@
     student with the highest score.
 */
 
+import java.util.Scanner;
 
 public class HighestScore{
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String bestStudent = "";
+        String name = "";
+        double score = 0;
+        double bestScore = -9999;
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = input.nextInt();
         
+        //input validation
+        if(numberOfStudents < 1){
+            System.out.println("There must be at least one student");
+        }
+
+        for(int i = 0; i < numberOfStudents; i++){
+            System.out.print("Enter the students name: ");
+            name = input.next();
+            System.out.print("Enter " + name + "'s score: ");
+            score = input.nextDouble();
+
+            if(score > bestScore){
+                bestStudent = name;
+                bestScore = score;
+            }
+        }
+
+        System.out.println("\n" + bestStudent + " is the best student with a " + 
+            "score of: " + bestScore + "\n\n");
     }
 }
 
