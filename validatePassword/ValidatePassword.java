ed6f910d2d919da22e5ed0be916099d222ca32a7
RustyLenovoLaptop <rustydegarmo@yahoo.com>
2022-10-21 10:35:58 -0500

    added a program to print the characters from 0 to z, and finished the ValidatePassword program.

--- validatePassword/ValidatePassword.java
+++ validatePassword/ValidatePassword.java
@@ -6,14 +6,25 @@
 
 import java.util.Scanner;
 
+
 public class ValidatePassword{
     public static void main(String[] args){
-    //test values: one should pass two should fail
+    //test values: one should pass two should fail three should fail
     String passOne = "1sometexthere2";
     String passTwo = "123text";
+    String passThree = "@124sometext";
 
-    testPassword(passOne);
-    testPassword(passTwo); 
+//    System.out.println("Test passwords: ");
+//    testPassword(passOne);
+//    testPassword(passTwo);
+//    testPassword(passThree);
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter your password: ");
+    String userPassword = input.nextLine();
+
+    testPassword(userPassword);
 
     }
 
@@ -28,15 +39,43 @@ public class ValidatePassword{
     }
 
     public static boolean validateLength(String pass){
-        return pass.length() > 8 ? true : false;
+        return pass.length() > 8;
     }
 
     public static boolean validateDigits(String pass){
-        return false;
+        int digitCounter = 0;
+
+        for(int i = 0; i < pass.length(); i++) {
+            if(pass.charAt(i) >= '0' && pass.charAt(i) <= '9'){
+                digitCounter++;
+            }
+        }
+
+
+        return digitCounter >= 2;
     }
 
     public static boolean validateAlphanumericOnly(String pass){
-        return false;
+        boolean alphaNumeric = false;
+
+        for(int i = 0; i < pass.length(); i++) {
+            if(pass.charAt(i) >= '0' && pass.charAt(i) <= '9'){
+                //character is a digit
+                alphaNumeric = true;
+            } else if(pass.charAt(i) >= 'A' && pass.charAt(i) <= 'Z'){
+                //character is upper case letter
+                alphaNumeric = true;
+            }else if (pass.charAt(i) >= 'a' && pass.charAt(i) <= 'z'){
+                //character is lower case letter
+                alphaNumeric = true;
+            }else{
+                //character is invalid
+                alphaNumeric = false;
+                return alphaNumeric;
+            }
+        }
+
+        return alphaNumeric;
     }
 
     
