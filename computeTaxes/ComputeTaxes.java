83ccbcf1a4ce61a44dd94d6bcf6312e595a0a739
RustyLenovoLaptop <rustydegarmo@yahoo.com>
2022-11-30 00:24:42 -0600

    started a program to compute taxes, implemented basic stubs

---
+++ computeTaxes/ComputeTaxes.java
@@ -0,0 +1,42 @@
+/*
+The purpose of this program is to compute taxes based on filing status
+and income. i.e. filing status single with annual income of 150,000. The 
+program will correctly compute how much tax is owed
+*/
+
+public class ComputeTaxes{
+	public static void main(String[] args){
+		int[] taxInfo = getTaxInformation();
+		
+		calculateTaxes(taxInfo);
+	}
+
+	//prompt the user to enter their filing status and annual income
+	public static int[] getTaxInformation(){
+		int[] info = {0, 0};
+		return info;
+	}
+
+	//calculate the taxes owed and display the information to the user
+	public static void calculateTaxes(int[] arr){
+		//rates and brackets are for the year 2022
+		double[] taxRates = {0.10, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37};
+
+		//each nested array is for a different filing status
+		//single, married/joint, married/seperate, head of house
+		int[][] taxBrackets = {
+			{11000, 44725, 95375, 182100, 231250, 578125}, 
+			{22000, 89450, 190750, 364200, 462500, 693750}, 
+			{11000, 44725, 95375, 182100, 231250, 346875}, 
+			{15700, 59850, 95350, 182100, 231250, 578100}
+		}
+
+		
+		printUserTaxes(arr);
+	}
+
+	//helper method to print the information to the console
+	public static void printUserTaxes(int[] arr){
+		System.out.println(arr.toString());
+	}
+}
