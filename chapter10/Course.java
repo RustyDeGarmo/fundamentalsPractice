56719ce5d25c129a03c20b8ef39b38c32e42f5e6
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2023-04-14 15:22:46 -0500

    added a Course class to add and remove students from a course and a program to test it

---
+++ chapter10/Course.java
@@ -0,0 +1,69 @@
+package chapter10;
+
+public class Course {
+    private String courseName;
+    private String[] students = new String[100];
+    private int numberOfStudents;
+
+    public Course(String courseName){
+        this.courseName = courseName;
+    }
+
+    public String[] getStudents(){
+        //return an array of students that is the size of the number of students 
+        //in the course
+        String[] newStudents = new String[numberOfStudents];
+        for(int i = 0; i < numberOfStudents; i++){
+            newStudents[i] = students[i];
+        }
+        return newStudents;
+    }
+
+    public int getNumberOfStudents(){
+        return numberOfStudents;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public void addStudent(String student){
+        if(students.length + 1 < numberOfStudents){
+            //if the array isn't big enough, create a new bigger array
+            String[] newStudents = new String[(int)(numberOfStudents * 1.1)];
+            for(int i = 0; i < students.length; i++){
+                newStudents[i] = students[i];
+            }
+            students = newStudents; //students is now the bigger array, the old 
+            //array is garbage. 
+        }
+        students[numberOfStudents] = student;
+        numberOfStudents++;
+        
+    }
+
+    public void dropStudent(String student){
+        //remove a student from the course
+        int index = 0;
+
+        for(int i = 0; i < students.length; i++){
+            if(students[i] == student){
+                students[i] = " ";
+                index = i;
+            }
+        }
+        //shift remaining students
+        for(int i = index; i < students.length - 1; i++){
+            students[i] = students[i+1];
+            students[i+1] = " "; //prevent the last student from being a duplicate
+        }
+
+    }
+    
+    public void clear(){
+        //remove all students from the course and reset to defaults
+        numberOfStudents = 100;
+        students = new String[numberOfStudents];
+    }
+
+}
