cce04d3f90300e10272987f7f4983d2d0a7ce3ff
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2023-04-30 15:23:19 -0500

    added a program that tests the Java File class methods. Also added a file that will be a web crawler. Implementation TODO.

---
+++ chapter12/WebCrawler.java
@@ -0,0 +1,8 @@
+package chapter12;
+/*
+ * This program will crawl the web starting from a given page. We will collect 
+ * URLs from the pages we visit until we have visited 100 pages.
+ */
+public class WebCrawler {
+    
+}
