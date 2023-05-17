ac6bcd6a72aed2912e523d1384bfa74b22102b8f
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2023-05-17 10:18:06 -0500

    added a program to test JavaFX in VS Code. The imports don't work. Or something doesn't anyway. Have to do some research to get VSC set up for JavaFX development

---
+++ chapter14/JavaFXTest.java
@@ -0,0 +1,27 @@
+package chapter14;
+
+/*
+ * This program is meant to test a basic JavaFX program with VS Code
+ */
+
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.stage.*;
+
+public class JavaFXTest extends Application{
+    @Override //override the start method from the Application class
+    public void start(Stage primaryStage){
+        //create a scene with a button
+        Button btOK = new Button("OK");
+        Scene scene = new Scene(btOK, 200, 250);
+        primaryStage.setTitle("Test JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    //main is only needed with certain IDE's. I don't know if VS Code needs 
+    //it so I'll just include it. It's always the same.
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+}
